f877144840dcdf0ef0b8aa988f2aa069a76c23f6
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-15 08:56:06 -1000

    [FEATURE] routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <RouterProvider
+      router={router}
+    />
+
   </React.StrictMode>,
 )
