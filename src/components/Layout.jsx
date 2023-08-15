49419e50c2bd89e7c2bf91c6206a0ac600c75834
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-15 08:55:09 -1000

    [FEATURE] layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom"
+
+
+import Header from "./Header"
+
+const Layout = () => {
+  return (
+    <div className='md:h-screen md:flex'>
+      <Header />
+
+      <main className="h-full py-1 md:w-3/4">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default Layout
